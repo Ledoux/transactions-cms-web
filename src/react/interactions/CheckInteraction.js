9ff2f7702f9031ac68c575f36d1fe04d763106f5
erwanledoux <lawanledoux@gmail.com>
2017-10-19 16:59:42 +0200

    made check interaction more flexible

--- src/react/interactions/CheckInteraction.js
+++ src/react/interactions/CheckInteraction.js
@@ -2,12 +2,21 @@ import React from 'react'
 import { IconButton } from 'transactions-interface-web'
 
 const CheckInteraction = ({ entityName,
+  icon,
+  isEdit,
   slug
 }) => {
+  let href = `/content/${entityName}/${slug}`
+  if (isEdit) {
+    href = `${href}?isEdit=true`
+  }
   return <IconButton className='button button--alive check-interaction'
-    icon='pen'
-    href={`/content/${entityName}/${slug}`}
-  />
+    icon={
+      icon || isEdit
+      ? 'pen'
+      : 'eye'
+    }
+    href={href} />
 }
 
 export default CheckInteraction
